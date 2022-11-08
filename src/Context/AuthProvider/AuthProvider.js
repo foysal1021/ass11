69dc68b18ebe089edb6415d66ac2b976d92f231b
foysal1021 <foysala1021@gmail.com>
2022-11-08 18:57:21 +0600

    setup

---
+++ src/Context/AuthProvider/AuthProvider.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ3aq_nrTaFcBeXZVd--5dU-YbHrT2Gzk",
+  authDomain: "medicalservices-cf64e.firebaseapp.com",
+  projectId: "medicalservices-cf64e",
+  storageBucket: "medicalservices-cf64e.appspot.com",
+  messagingSenderId: "973835229987",
+  appId: "1:973835229987:web:c800e9af06c36cc772ca96",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
