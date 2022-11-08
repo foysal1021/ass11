9daafea470ee35d35377aba4108e414c8b1c5706
foysal1021 <foysala1021@gmail.com>
2022-11-08 19:57:41 +0600

    added a banner

--- src/Component/ShardPage/Header/Header.js
+++ src/Component/ShardPage/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "../../Home/Banner";
 import Nav from "../../Home/Nav";
 
 const Header = () => {
   return (
     <div>
       <Nav></Nav>
+      <Banner></Banner>
     </div>
   );
 };
